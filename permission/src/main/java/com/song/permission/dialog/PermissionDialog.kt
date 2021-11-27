7022da97dbcc0067245343a6d44f2e1dbd6177b7
mac-song <454104931@qq.com>
2021-11-27 14:41:38 +0800

    优化

--- permission/src/main/java/com/song/permission/dialog/PermissionDialog.kt
+++ permission/src/main/java/com/song/permission/dialog/PermissionDialog.kt
@@ -5,9 +5,9 @@ import android.content.Context
 import android.view.View
 
 abstract class PermissionDialog(context: Context):Dialog(context){
+
     var permissionsList:Array<String> = arrayOf()
     var deniedList: MutableList<String> = mutableListOf()
-
 
     abstract fun getCancelView():View
     abstract fun getConfirmView():View
